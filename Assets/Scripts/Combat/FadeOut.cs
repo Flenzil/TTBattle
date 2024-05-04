8a0009b4e7985c5e82910d9d64b018abcd421ed5
Flenzil <plimfelt@hotmail.com>
2024-05-04 13:19:56 +0100

    Creatures fade and delete after shattering

--- Assets/Scripts/Combat/FadeOut.cs
+++ Assets/Scripts/Combat/FadeOut.cs
@@ -1,6 +1,10 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
 using Unity.VisualScripting;
+using UnityEditor.SceneManagement;
 using UnityEngine;
 using UnityEngine.Rendering.HighDefinition;
 
@@ -8,45 +12,65 @@ public class FadeOut : MonoBehaviour
 {
     //[SerializeField] Material fadeMaterial;
 
-    public float fadeSpeed = 0.01f;
-    public float fadeDelay = 5;
+    private float fadeSpeed = 1f;
+    private float fadeDelay = 5;
     private Renderer rend;
+    private MeshFilter meshFilter;
+    private Color[] colours;
+    private bool isRunning = false;
 
     void Start(){
         rend = GetComponent<Renderer>();
+        meshFilter = GetComponent<MeshFilter>();
+
+        HDMaterial.SetSurfaceType(rend.material, true);
+        rend.material.color = new Color(rend.material.color.r, rend.material.color.g, rend.material.color.b, 1);
+        for (int i = 0; i < transform.childCount; i++){
+            HDMaterial.SetSurfaceType(transform.GetChild(i).GetComponent<Renderer>().material, true);
+            transform.GetChild(i).GetComponent<Renderer>().material.color = new Color(rend.material.color.r, rend.material.color.g, rend.material.color.b, 1);
+        }
+
+        colours = colourList();
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        StartCoroutine(Fade());
+        if (!isRunning){
+            StartCoroutine(Fade());
+            isRunning = true;
+        }
     }
-    
+
     private IEnumerator Fade(){
+
         yield return new WaitForSeconds(fadeDelay);
 
-        HDMaterial.SetSurfaceType(rend.material, true);
+        GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeAll;
 
-        while (true){
-            Color colour = rend.material.GetColor("_BaseColor");
-            Color newColour = new Color(colour.r, colour.g, colour.b, colour.a - fadeSpeed * Time.deltaTime);
-            rend.material.SetColor("_BaseColor", newColour);
-            if (rend.material.GetColor("_BaseColor").a <= 0){
-                Destroy(gameObject);
-                yield break;
+        foreach (Color colour in colours){
+
+            rend.material.SetColor("_BaseColor", colour);
+            for (int i = 0; i < transform.childCount; i++){
+                transform.GetChild(i).GetComponent<Renderer>().material.SetColor("_BaseColor", colour);
             }
             yield return null;
         }
 
-        /*
-        for (float alpha = 1f; alpha >= 0; alpha -= fadeSpeed){
-            colour.a = alpha;
-            //rend.sharedMaterial.color = colour;
-            rend.material.SetColor("_BaseColor", new Color(1,1,1,alpha));
-            yield return null;
+        Destroy(gameObject);
+        yield break;
+    }
+
+    private Color[] colourList(){
+        Color colour = rend.material.GetColor("_BaseColor");
+        Color colourStart = new Color(colour.r, colour.g, colour.b, 1);
+        Color colourEnd = new Color(colour.r, colour.g, colour.b, 0);
+        Color[] lerp = new Color[(int)(1 / (fadeSpeed * Time.deltaTime))];
+
+        for (int i = 0; i < lerp.Count() ; i++){
+            lerp[i] = Color.Lerp(colourStart, colourEnd, i * (fadeSpeed * Time.deltaTime));
         }
-        */
-        //Destroy(gameObject);
+        return lerp;
     }
 }
-
--- Assets/Scripts/Combat/Shatter.cs
+++ Assets/Scripts/Combat/Shatter.cs
@@ -8,18 +8,12 @@ using UnityEngine.UIElements;
 public class Shatter : MonoBehaviour {
 
     public float pointDensity = 0.1f;
-    public float explosionForce = 500;
+    public float explosionForce = 1000;
     public float fadeSpeed = 0.1f;
     
     [SerializeField] GameObject shatterPieces;
 
-    private Collider col;
     private GameObject shatteredObject;
-
-
-    void Start(){
-        col = GetComponent<MeshCollider>();
-    }
 
     public void Kill(int health){
         float critMultiplier = 1;
@@ -35,14 +29,12 @@ public class Shatter : MonoBehaviour {
             shatteredObject.transform.position,
             300f
         );
-        shatteredObject.AddComponent<FadeOut>();
         for (int i = 0; i < shatteredObject.transform.childCount; i++){
             shatteredObject.transform.GetChild(i).GetComponent<Rigidbody>().AddExplosionForce(
                 explosionForce,
                 shatteredObject.transform.position,
                 300f
             );
-            shatteredObject.transform.GetChild(i).AddComponent<FadeOut>();
         }
         Destroy(gameObject);
     }
